7a1857d9bd122f238560ec9493b3f035456d5122
Void10100 <ponomarenkogleb916@gmail.com>
2026-03-01 19:28:46 +0200

    поправил варпы

--- start_round.lua
+++ start_round.lua
@@ -1,14 +1,22 @@
 warps = 10
 
 local playerDetector = peripheral.find("playerDetector")
-local acceptedWarps = {}
-for i = 1, warps do
-    table.insert(acceptedWarps, i)
+local currentPlayers = playerDetector.getOnlinePlayers()
+
+local function startRound(players)
+    local acceptedWarps = {}
+    for i = 1, warps do
+        table.insert(acceptedWarps, i)
+    end
+
+    for _, player in ipairs(players) do
+        if #acceptedWarps == 0 then return end
+
+        local randomIndex = math.random(1, #acceptedWarps)
+        local warp = acceptedWarps[randomIndex]
+        commands.exec(string.format("execute as %s run warp %d", player, warp))
+        table.remove(acceptedWarps, randomIndex)
+    end
 end
 
-local players = playerDetector.getOnlinePlayers()
-for _, player in ipairs(players) do
-    local warp = acceptedWarps[math.floor(math.random(1, #acceptedWarps))]
-    commands.exec(string.format("execute as %s run warp %d"), player, warp)
-    table.remove(acceptedWarps, table.find(acceptedWarps, warp))
-end
+startRound(currentPlayers)
